4b45b1f06279eb5a3b6e3c848bdbd0be8719f415
AMoralt <dias_galym@bk.ru>
2024-09-06 15:05:19 +0500

    Добавил библиотеку для реализации инъекции зависимостей. Написал в program.cs эмуляцию работы с классами.

--- Test assignment/Program.cs
+++ Test assignment/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Test_assignment;
 
-Console.WriteLine("Hello, World!");
+var serviceCollection = new ServiceCollection();
+ConfigureServices(serviceCollection);
+
+// Создание провайдера сервисов
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+// Использование сервисов
+var moneyOperations = serviceProvider.GetService<IMoneyOperations>();
+
+Money usdMoney = new Money(100, "USD");
+Money eurMoney = new Money(85, "EUR");
+
+if (moneyOperations != null)
+{
+    var result = moneyOperations.Add(usdMoney, eurMoney);
+    Console.WriteLine($"{result.Amount} {result.CurrencyCode}");
+}
+
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<IExchangeRateFactory, ExchangeRateFactory>();
+        
+    services.AddTransient<IExchangeRateProvider, ExchangeRateProvider>();
+    
+    services.AddTransient<ICurrencyConverter, CurrencyConverter>();
+    
+    services.AddTransient<IMoneyOperations, MoneyOperations>();
+}
